1f0e1e54655cec834168f780c0da6c9a60666a2a
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-08-07 18:25:00 -0400

    changed the data insertion method

--- todolist.sql
+++ todolist.sql
@@ -7,18 +7,21 @@ CREATE TABLE todolist (
     completed_at DATE
   );
 
+  INSERT INTO todolist (title, details, priority, created_at, completed_at) VALUES
+
+    ('make coffee', 'go to kitchen and brew coffee', 2, '2017-08-07', '2017-08-07'
+    ),
+
+    ('buy tiy', 'save three million dollars and buy the school.', 3, '2017-07-15', null),
+
+    ('visit friend', 'call him and go.', 4,   '2017-08-02', null),
+
+    ('watch movie', 'rent a movie from RedBox', 5, '2017-08-12', null),
+
+    ('finish project', 'work on the daily assigment', 1, '2017-08-07', '2017-08-08');
 
 
-  INSERT INTO todolist (title, details, priority, created_at) VALUES ('buy tiy', 'save three million dollars and buy the school.', 3, '2017-07-15');
 
-  INSERT INTO todolist (title, details, priority, created_at, completed_at) VALUES ('make coffee', 'go to kitchen and brew coffee', 2, '2017-08-07', '2017-08-07'
-  );
-
-  INSERT INTO todolist (title, details, priority, created_at) VALUES ('visit friend', 'call him and go.', 4, '2017-08-02');
-
-  INSERT INTO todolist (title, details, priority, created_at) VALUES ('watch movie', 'rent a movie from RedBox', 5, '2017-08-12');
-
-  INSERT INTO todolist (title, details, priority, created_at, completed_at) VALUES ('finish project', 'work on the daily assigment', 1, '2017-08-07', '2017-08-08');
 
   select * from todolist where completed_at is null;
 
@@ -27,3 +30,5 @@ CREATE TABLE todolist (
   UPDATE todolist set completed_at = '2017-08-13' WHERE id = 3;
 
   DELETE FROM todolist WHERE completed_at is NOT NUll;
+
+  ALTER TABLE todolist ALTER COLUMN completed_at type Null;
